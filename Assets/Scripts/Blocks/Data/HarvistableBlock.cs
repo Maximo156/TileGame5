f07640d3834f7c03711a72d389edee86f5b6ff56
Maximo156 <turnermaximo@gmail.com>
2025-12-31 17:19:05 -0500

    add apple tree and rassberry bush

--- Assets/Scripts/Blocks/Data/HarvistableBlock.cs
+++ Assets/Scripts/Blocks/Data/HarvistableBlock.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewHarvistableBlock", menuName = "Block/HarvistableBlock", order = 1)]
 public class HarvistableBlock : Wall, IInteractableBlock
 {
+    [Header("Harvest Info")]
     public Block AfterHarvest;
+    public bool useOverrides;
+    public List<ItemStack> HarvestOverrides;
+
     public bool Interact(Vector2Int worldPos, BlockSlice slice)
     {
         if (AfterHarvest != null)
         {
             slice.SetBlock(AfterHarvest);
-            Utilities.DropItems(worldPos, Drops);
+            Utilities.DropItems(worldPos, useOverrides ? HarvestOverrides : Drops);
         }
         else
         {
