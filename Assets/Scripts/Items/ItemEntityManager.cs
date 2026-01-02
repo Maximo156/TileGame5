88a985c00f09f8efaca72626402bc44e97504fd0
Negin.Sajedi <Negin.Sajedi@bentley.com>
2026-01-02 16:06:25 -0500

    Disable item pickup when inventory is full

--- Assets/Scripts/Items/ItemEntityManager.cs
+++ Assets/Scripts/Items/ItemEntityManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// Keeps track of all the active Items
 public class ItemEntityManager : MonoBehaviour
 {
     public GameObject ItemEntityPrefab;
--- Assets/Scripts/Player/ItemPickup.cs
+++ Assets/Scripts/Player/ItemPickup.cs
@@ -1,15 +1,16 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using EntityStatistics;
 
+/// <summary>
+/// Pulls the items within a given range
+/// </summary>
 public class ItemPickup : MonoBehaviour
 {
     public float forceFactor = 30f;
     public CircleCollider2D col;
     
+    PlayerInventories _playerInventory;
     private List<Rigidbody2D> _pickedUpItems = new();
     private EntityStats _entityStats;
 
@@ -17,6 +18,7 @@ public class ItemPickup : MonoBehaviour
     {
         _entityStats = GetComponentInParent<EntityStats>();
         col.radius = _entityStats.GetStat(EntityStats.Stat.PickupRange);
+        _playerInventory = GetComponentInParent<PlayerInventories>();
         _entityStats.OnStatChanged += UpdateRange;
     }
 
@@ -33,17 +35,23 @@ public class ItemPickup : MonoBehaviour
         var currPos = transform.position.ToVector2();
         foreach (var itemBody in _pickedUpItems)
         {
-            if (itemBody == null)
+            if (!itemBody)
                 continue;
-            var dir = (currPos - itemBody.position);
-            itemBody.AddForce(dir.normalized * (forceFactor * Time.fixedDeltaTime)/(dir.magnitude));
+            
+            var itemEntity = itemBody.GetComponent<ItemEntity>();
+            if (_playerInventory.MainInv.CanAddItem(itemEntity.stack))
+            {
+                var dir = (currPos - itemBody.position);
+                itemBody.AddForce(dir.normalized * (forceFactor * Time.fixedDeltaTime)/(dir.magnitude));
+            }
         }
 
-        _pickedUpItems.RemoveAll(x => x == null);
+        _pickedUpItems.RemoveAll(x => !x);
     }
     
     
     //Detect collisions between the GameObjects with Colliders attached
+    // TODO: Check tag
     void OnTriggerEnter2D(Collider2D collider)
     {
         var test = collider.GetComponent<Rigidbody2D>();
