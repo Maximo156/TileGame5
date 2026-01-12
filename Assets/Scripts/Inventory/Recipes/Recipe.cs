30964401df87b9b83b8de13f5e3d4fffb7d0b141
Maximo156 <turnermaximo@gmail.com>
2026-01-11 21:32:25 -0500

    Fix bool name

--- Assets/Scripts/Inventory/Recipes/Recipe.cs
+++ Assets/Scripts/Inventory/Recipes/Recipe.cs
@@ -9,7 +9,7 @@ public abstract class Recipe: IGridItem, IGridSource
 
     public bool CanProduce(IEnumerable<ItemStack> items, bool skipRecipePossible = false)
     {
-        if (skipRecipePossible && WorldSettings.UseRecipeInputs) return true;
+        if (skipRecipePossible && WorldSettings.DontUseRecipeInputs) return true;
         var dict = Utilities.ConvertToItemCounts(items);
         foreach (var itemstack in Required)
         {
@@ -23,7 +23,7 @@ public abstract class Recipe: IGridItem, IGridSource
 
     public void UseRecipe(IInventoryContainer inventory, bool skipRecipePossible = false)
     {
-        if (skipRecipePossible && WorldSettings.UseRecipeInputs) return;
+        if (skipRecipePossible && WorldSettings.DontUseRecipeInputs) return;
         foreach(var item in Required)
         {
             inventory.RemoveItemSafe(item);
