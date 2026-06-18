db6b5a99b1957be0bb1e4bd2230751b5b512ce2a
Maximo156 <turnermaximo@gmail.com>
2026-06-18 19:37:19 -0400

    Faster sound 1

--- Assets/Scripts/World/Sound/FractalSoundSettings.cs
+++ Assets/Scripts/World/Sound/FractalSoundSettings.cs
@@ -4,6 +4,7 @@ using Unity.Mathematics;
 using Unity.Collections;
 using Unity.Jobs;
 using System;
+using System.Runtime.CompilerServices;
 
 [CreateAssetMenu(fileName = "NewFractalSoundSettings", menuName = "Terrain/FractalSoundSettings", order = 1)]
 [BurstCompile]
